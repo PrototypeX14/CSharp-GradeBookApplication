cfff9f63d8feb437b3aed4484e22f81faf554d66
PrototypeX14 <ProtoX14537@gmail.com>
2021-02-17 10:00:18 +1100

    Overrode CalculateStudentStatistics in RankedGradeBook.

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -22,16 +22,12 @@ namespace GradeBook.GradeBooks
 
             if (grades[threshold - 1] <= averageGrade)
                 return 'A';
-
             if (grades[(threshold * 2) - 1] <= averageGrade)
                 return 'B';
-
             if (grades[(threshold * 3) - 1] <= averageGrade)
                 return 'C';
-
             if (grades[(threshold * 4) - 1] <= averageGrade)
                 return 'D';
-
             return 'F';
         }
 
@@ -44,6 +40,17 @@ namespace GradeBook.GradeBooks
             }
             
             base.CalculateStatistics();
+        }
+
+        public override void CalculateStudentStatistics(string name)
+        {
+            if (Students.Count() < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+                return;
+            }
+            
+            base.CalculateStudentStatistics(name);
         }
     }
 }
